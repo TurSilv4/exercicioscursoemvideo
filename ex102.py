ccca3c4e2313cc5f61e24ea45b8daa9c1e915230
Artur_Dev? <arturrdev.contact@gmail.com>
2022-06-05 14:31:16 -0300

    Ex102

--- ex102.py
+++ ex102.py
@@ -3,15 +3,15 @@
 # Indicando se sera mostrado ou não na tela de processo de cálculo do fatorial
 
 
-def fatorial(num, show=bool(input('Ver resolução? True/False'))):
+def fatorial(num):
     f = 1
 
     for cont in range(num, 0, -1):
         f *= cont
     if show == True:
         for cont in range(num, 0, -1):
-            print(cont, end=' x 'if cont > 0 else)
-        print(' = ', end='')
+            print(f'{cont}')
+        print(' + ' if cont > 0 else ' = ')
     return f
 
 
