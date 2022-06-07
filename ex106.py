998cace5f4604e20df0124bcc465fe83e8522d2a
Artur_Dev? <arturrdev.contact@gmail.com>
2022-06-07 18:42:58 -0300

    Imcompleto

---
+++ ex106.py
@@ -0,0 +1,15 @@
+# Fazer um mini-sistema que utilize o interactive help. O Usuario vai digitar o comando e o manual vai aparecer.
+# Quando digitar a palavra FIM o programa se encerra // Use cores
+
+def cabeçalho(msg):
+    print('\033[43m~' * len(msg+2))
+    print('')
+    print('~\033[m' * len(msg+2))
+
+
+
+
+
+
+# Programa principal
+
