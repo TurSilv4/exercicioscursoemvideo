63083fa856a0896adfad4f84d1e5f15abadd424e
Artur_Dev? <arturrdev.contact@gmail.com>
2022-06-08 17:12:53 -0300

    Completo

--- ex113/ex113.py
+++ ex113/ex113.py
@@ -27,8 +27,3 @@ def leiaFloat(msg):
             return 0
         else:
             return n
-
-
-int = leiaInt('Digite um inteiro: ')
-float = leiaFloat('Digite um Real: ')
-print(f'O valor inteiro digitado foi {int} e o real foi {float}')
