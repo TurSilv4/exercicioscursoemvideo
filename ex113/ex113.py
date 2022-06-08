6c5d21075a01f28d9492c94b8f0a6f4c5168a60c
Artur_Dev? <arturrdev.contact@gmail.com>
2022-06-08 17:12:34 -0300

    Completo

---
+++ ex113/ex113.py
@@ -0,0 +1,34 @@
+# Re-escreva a função leiaInt() do ex104, incluindo agora a possibilidade de digitalização de um numero de tipo invalido.
+# Aproveite e crie tambem uma função leiaFloat() com a mesma funcionalidade
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (TypeError, ValueError):
+            print('\033[0;31mO valor informado é inválido!\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\n\033[0;31mO Usuario preferiu não informar o valor!\033[m')
+            return 0
+        else:
+            return n
+
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (TypeError, ValueError):
+            print('\033[0;31mO valor informado é inválido!\033[m')
+        except KeyboardInterrupt:
+            print('\033[0;31m\nO usuario preferiu não informar esse numero\033[m')
+            return 0
+        else:
+            return n
+
+
+int = leiaInt('Digite um inteiro: ')
+float = leiaFloat('Digite um Real: ')
+print(f'O valor inteiro digitado foi {int} e o real foi {float}')
