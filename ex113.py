382183461a944ab463ef59286cdd36f99b14823b
Artur_Dev? <arturrdev.contact@gmail.com>
2022-06-08 17:16:59 -0300

    Completo

---
+++ ex113.py
@@ -0,0 +1,5 @@
+from ex113_uteis import ex113
+
+int = ex113.leiaInt('Digite um inteiro: ')
+float = ex113.leiaFloat('Digite um Real: ')
+print(f'O valor inteiro digitado foi {int} e o real foi {float}')
